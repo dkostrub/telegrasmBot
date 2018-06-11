fb7d9a4162094bcc080cd4db02f5425aa58c6e65
Dmitriy Kostrub <dmitriy.kostrub@forforce.com>
2018-06-11 14:08:33 +0300

    test

---
+++ wb.php
@@ -0,0 +1,11 @@
+<?php
+
+const TOKEN = '570281200:AAGF3hdXo3oSJpzHkRIfOUlGkQvVembmZOc';
+$method = 'setWebhook';
+
+$url = 'https://api.telegram.org/bot'.TOKEN.'/'.$method;
+
+$options = ['url' => 'https://telegrambot.loc/index.php'];
+$response = file_get_contents($url . '?' . http_build_query($options));
+
+var_dump($response);
