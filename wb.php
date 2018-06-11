2b19718e9dbb24506aff313c9441b35d3b45a5b1
Dmitriy Kostrub <dmitriy.kostrub@forforce.com>
2018-06-11 14:14:51 +0300

    test

--- wb.php
+++ wb.php
@@ -5,7 +5,7 @@ $method = 'setWebhook';
 
 $url = 'https://api.telegram.org/bot'.TOKEN.'/'.$method;
 
-$options = ['url' => 'https://telegrambot.loc/index.php'];
+$options = ['url' => 'https://dinnerbotnoti.herokuapp.com/index.php'];
 $response = file_get_contents($url . '?' . http_build_query($options));
 
 var_dump($response);
