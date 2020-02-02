0f6eaa19aa1c31bbad9b90711ca30afb90e8da89
Dmitriy Kostrub <dmitriy.kostrub@forforce.com>
2020-02-02 10:17:16 +0200

    test webhook

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
+
+require_once("vendor/autoload.php");
+
 const TOKEN = '570281200:AAGF3hdXo3oSJpzHkRIfOUlGkQvVembmZOc';
+$bot = new \TelegramBot\Api\Client(TOKEN);
 const BASE_URL = 'https://api.telegram.org/bot'.TOKEN.'/';
 
-$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+// если бот еще не зарегистрирован - регистрируем
+if(!file_exists("registered.trigger")){
+    /**
+     * файл registered.trigger будет создаваться после регистрации бота.
+     * если этого файла нет значит бот не зарегистрирован
+     */
 
-function sendRequest($method, $params = [])
-{
-    if(!empty($params)){
-        $url = BASE_URL . $method . '?' . http_build_query($params);
-    } else {
-        $url = BASE_URL . $method;
+    // URl текущей страницы
+    $page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    $result = $bot->setWebhook($page_url);
+    if($result){
+        file_put_contents("registered.trigger",time()); // создаем файл дабы прекратить повторные регистрации
     }
-
-    return json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
 }
 
-$time = date('H:m:s');
-$chat_id = $update['message']['chat']['id'];
-sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $time]);
+//$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+//
+//function sendRequest($method, $params = [])
+//{
+//    if(!empty($params)){
+//        $url = BASE_URL . $method . '?' . http_build_query($params);
+//    } else {
+//        $url = BASE_URL . $method;
+//    }
+//
+//    return json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+//}
+//
+//$time = date('H:m:s');
+//$chat_id = $update['message']['chat']['id'];
+//sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $time]);
